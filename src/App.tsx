3249079b4a68c351afe50284cb53b05c94e52c18
Mohamad Reda <dragoongames24@gmail.com>
2026-04-19 17:25:07 +0200

    seting up the API and making the movie section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Search from "./Components/Search";
 
 function App() {
   const [query, setquery] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+
+  const API_BASE_URL = "https://api.themoviedb.org/3";
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+  useEffect(() => {
+    const API_OPTION = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    };
+
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      setErrorMsg("");
+
+      try {
+        const endPoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+        const response = await fetch(endPoint, API_OPTION);
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch movies");
+        }
+
+        const data = await response.json();
+
+        if (data.results === "false") {
+          setErrorMsg(data.Error || "Failed to fetch movies");
+        } else {
+          setMovies(data.results || []);
+          setIsLoading(false);
+          return;
+        }
+      } catch (e) {
+        console.error("Error fetching movies:", e);
+        setErrorMsg(
+          "there was an error fetching movies, please try again later",
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, [API_KEY]);
+
   return (
     <main>
       <div className="pattern" />
@@ -14,9 +65,27 @@ function App() {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
+          <Search query={query} setQuery={setquery} />
         </header>
 
-        <Search query={query} setQuery={setquery} />
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {isLoading ? (
+            <p className="text-white">Loading..... Wait bihh</p>
+          ) : errorMsg ? (
+            <p className="text-red-600">{errorMsg}</p>
+          ) : (
+            <ul>
+              {movies.map((movie) => {
+                return (
+                  <p key={movie.id} className="text-white">
+                    {movie.title}
+                  </p>
+                );
+              })}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   );
